c34348ab845d9b5c41df04968568e14cbec5dc57
David Fischer <david@fisch3r.net>
2020-04-07 16:29:32 +0200

    Refresh fields of resource_job_template

--- awx/resource_job_template.go
+++ awx/resource_job_template.go
@@ -21,15 +21,16 @@ func resourceJobTemplateObject() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"name": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Name of this job template.",
 			},
 			"description": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Default:  "",
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "",
+				Description: "Optional description of this job template.",
 			},
-			// Run, Check, Scan
 			"job_type": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
@@ -53,14 +54,11 @@ func resourceJobTemplateObject() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
-			"extra_credential_ids": &schema.Schema{
-				Type:     schema.TypeList,
-				Optional: true,
-				Elem:     &schema.Schema{Type: schema.TypeInt},
-			},
-			"vault_credential_id": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
+			"scm_branch": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "",
+				Description: "Branch to use in job run. Project default used if blank. Only allowed if project allow_override field is set to true.",
 			},
 			"forks": &schema.Schema{
 				Type:     schema.TypeInt,
@@ -72,12 +70,11 @@ func resourceJobTemplateObject() *schema.Resource {
 				Optional: true,
 				Default:  "",
 			},
-			//0,1,2,3,4,5
 			"verbosity": &schema.Schema{
 				Type:        schema.TypeInt,
 				Optional:    true,
 				Default:     0,
-				Description: "One of 0,1,2,3,4,5",
+				Description: "In range 0-5 (Normal, Verbose, More Verbose, Debug, Connection Debug, WinRM Debug)",
 			},
 			"extra_vars": &schema.Schema{
 				Type:     schema.TypeString,
@@ -105,21 +102,33 @@ func resourceJobTemplateObject() *schema.Resource {
 				Default:  "",
 			},
 			"timeout": &schema.Schema{
-				Type:     schema.TypeInt,
-				Optional: true,
-				Default:  0,
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     0,
+				Description: "The amount of time (in seconds) to run before the task is canceled.",
 			},
 			"use_fact_cache": &schema.Schema{
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "If enabled, Tower will act as an Ansible Fact Cache Plugin; persisting facts at the end of a playbook run to the database and caching facts for use by Ansible.",
 			},
 			"host_config_key": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
 				Default:  "",
 			},
+			"ask_scm_branch_on_launch": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
 			"ask_diff_mode_on_launch": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"ask_variables_on_launch": &schema.Schema{
 				Type:     schema.TypeBool,
 				Optional: true,
 				Default:  false,
@@ -134,6 +143,16 @@ func resourceJobTemplateObject() *schema.Resource {
 				Optional: true,
 				Default:  false,
 			},
+			"ask_skip_tags_on_launch": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"ask_job_type_on_launch": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
 			"ask_verbosity_on_launch": &schema.Schema{
 				Type:     schema.TypeBool,
 				Optional: true,
@@ -144,11 +163,6 @@ func resourceJobTemplateObject() *schema.Resource {
 				Optional:      true,
 				Default:       false,
 				ConflictsWith: []string{"inventory_id"},
-			},
-			"ask_variables_on_launch": &schema.Schema{
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
 			},
 			"ask_credential_on_launch": &schema.Schema{
 				Type:     schema.TypeBool,
@@ -166,14 +180,10 @@ func resourceJobTemplateObject() *schema.Resource {
 				Default:  false,
 			},
 			"diff_mode": &schema.Schema{
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
-			},
-			"ask_skip_tags_on_launch": &schema.Schema{
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "If enabled, textual changes made to any templated files on the host are shown in the standard output.",
 			},
 			"allow_simultaneous": &schema.Schema{
 				Type:     schema.TypeBool,
@@ -181,19 +191,44 @@ func resourceJobTemplateObject() *schema.Resource {
 				Default:  false,
 			},
 			"custom_virtualenv": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Default:  "",
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "",
+				Description: "Local absolute file path containing a custom Python virtualenv to use.",
 			},
-			"ask_job_type_on_launch": &schema.Schema{
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
+			"job_slice_count": &schema.Schema{
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     1,
+				Description: "The number of jobs to slice into at runtime. Will cause the Job Template to launch a workflow if value is greater than 1.",
 			},
+			"webhook_service": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "",
+				Description: "Service that webhook requests will be accepted from (github or gitlab)",
+			},
+			"webhook_credential_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Sensitive:   true,
+				Description: "Personal Access Token for posting back the status to the service API.",
+			},
+
+			// Extra fields (such as self identifier)
 			"job_id": {
 				Type:     schema.TypeInt,
 				Optional: true,
 				Default:  0,
+			},
+			"extra_credential_ids": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeInt},
+			},
+			"vault_credential_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
 			},
 		},
 
@@ -253,6 +288,8 @@ func resourceJobTemplateCreate(d *schema.ResourceData, m interface{}) error {
 		"inventory":                AtoipOr(d.Get("inventory_id").(string), nil),
 		"project":                  AtoipOr(d.Get("project_id").(string), nil),
 		"playbook":                 d.Get("playbook").(string),
+		"credential":               AtoipOr(d.Get("credential_id").(string), nil),
+		"scm_branch":               d.Get("scm_branch").(string),
 		"forks":                    d.Get("forks").(int),
 		"limit":                    d.Get("limit").(string),
 		"verbosity":                d.Get("verbosity").(int),
@@ -264,6 +301,7 @@ func resourceJobTemplateCreate(d *schema.ResourceData, m interface{}) error {
 		"timeout":                  d.Get("timeout").(int),
 		"use_fact_cache":           d.Get("use_fact_cache").(bool),
 		"host_config_key":          d.Get("host_config_key").(string),
+		"ask_scm_branch_on_launch": d.Get("ask_scm_branch_on_launch").(bool),
 		"ask_diff_mode_on_launch":  d.Get("ask_diff_mode_on_launch").(bool),
 		"ask_variables_on_launch":  d.Get("ask_variables_on_launch").(bool),
 		"ask_limit_on_launch":      d.Get("ask_limit_on_launch").(bool),
@@ -278,7 +316,9 @@ func resourceJobTemplateCreate(d *schema.ResourceData, m interface{}) error {
 		"diff_mode":                d.Get("diff_mode").(bool),
 		"allow_simultaneous":       d.Get("allow_simultaneous").(bool),
 		"custom_virtualenv":        AtoipOr(d.Get("custom_virtualenv").(string), nil),
-		"credential":               AtoipOr(d.Get("credential_id").(string), nil),
+		"job_slice_count":          d.Get("job_slice_count").(int),
+		"webhook_service":          d.Get("webhook_service").(string),
+		"webhook_credential":       AtoipOr(d.Get("webhook_credential_id").(string), nil),
 		"vault_credential":         AtoipOr(d.Get("vault_credential_id").(string), nil),
 	}
 
@@ -326,6 +366,8 @@ func resourceJobTemplateUpdate(d *schema.ResourceData, m interface{}) error {
 		"inventory":                AtoipOr(d.Get("inventory_id").(string), nil),
 		"project":                  AtoipOr(d.Get("project_id").(string), nil),
 		"playbook":                 d.Get("playbook").(string),
+		"credential":               AtoipOr(d.Get("credential_id").(string), nil),
+		"scm_branch":               d.Get("scm_branch").(string),
 		"forks":                    d.Get("forks").(int),
 		"limit":                    d.Get("limit").(string),
 		"verbosity":                d.Get("verbosity").(int),
@@ -337,6 +379,7 @@ func resourceJobTemplateUpdate(d *schema.ResourceData, m interface{}) error {
 		"timeout":                  d.Get("timeout").(int),
 		"use_fact_cache":           d.Get("use_fact_cache").(bool),
 		"host_config_key":          d.Get("host_config_key").(string),
+		"ask_scm_branch_on_launch": d.Get("ask_scm_branch_on_launch").(bool),
 		"ask_diff_mode_on_launch":  d.Get("ask_diff_mode_on_launch").(bool),
 		"ask_variables_on_launch":  d.Get("ask_variables_on_launch").(bool),
 		"ask_limit_on_launch":      d.Get("ask_limit_on_launch").(bool),
@@ -351,7 +394,9 @@ func resourceJobTemplateUpdate(d *schema.ResourceData, m interface{}) error {
 		"diff_mode":                d.Get("diff_mode").(bool),
 		"allow_simultaneous":       d.Get("allow_simultaneous").(bool),
 		"custom_virtualenv":        AtoipOr(d.Get("custom_virtualenv").(string), nil),
-		"credential":               AtoipOr(d.Get("credential_id").(string), nil),
+		"job_slice_count":          d.Get("job_slice_count").(int),
+		"webhook_service":          d.Get("webhook_service").(string),
+		"webhook_credential":       AtoipOr(d.Get("webhook_credential_id").(string), nil),
 		"vault_credential":         AtoipOr(d.Get("vault_credential_id").(string), nil),
 	}, map[string]string{})
 	if err != nil {
@@ -420,33 +465,44 @@ func setJobTemplateResourceData(d *schema.ResourceData, r *awxgo.JobTemplate) *s
 	d.Set("job_id", r.ID)
 	d.Set("allow_simultaneous", r.AllowSimultaneous)
 	d.Set("ask_credential_on_launch", r.AskCredentialOnLaunch)
+	d.Set("ask_diff_mode_on_launch", r.AskDiffModeOnLaunch)
+	d.Set("ask_inventory_on_launch", r.AskInventoryOnLaunch)
 	d.Set("ask_job_type_on_launch", r.AskJobTypeOnLaunch)
 	d.Set("ask_limit_on_launch", r.AskLimitOnLaunch)
+	d.Set("ask_scm_branch_on_launch", r.AskScmBranchOnLaunch)
 	d.Set("ask_skip_tags_on_launch", r.AskSkipTagsOnLaunch)
 	d.Set("ask_tags_on_launch", r.AskTagsOnLaunch)
 	d.Set("ask_variables_on_launch", r.AskVariablesOnLaunch)
+	d.Set("ask_verbosity_on_launch", r.AskVerbosityOnLaunch)
+	d.Set("become_enabled", r.BecomeEnabled)
+	d.Set("become_enabled", r.BecomeEnabled)
 	d.Set("credential_id", r.Credential)
+	d.Set("custom_virtualenv", r.CustomVirtualenv)
 	d.Set("description", r.Description)
+	d.Set("diff_mode", r.DiffMode)
+	d.Set("diff_mode", r.DiffMode)
 	d.Set("extra_vars", r.ExtraVars)
 	d.Set("force_handlers", r.ForceHandlers)
 	d.Set("forks", r.Forks)
 	d.Set("host_config_key", r.HostConfigKey)
 	d.Set("inventory_id", r.Inventory)
+	d.Set("job_slice_count", r.JobSliceCount)
 	d.Set("job_tags", r.JobTags)
 	d.Set("job_type", r.JobType)
-	d.Set("diff_mode", r.DiffMode)
-	d.Set("custom_virtualenv", r.CustomVirtualenv)
-	d.Set("vault_credential_id", r.VaultCredential)
 	d.Set("limit", r.Limit)
 	d.Set("name", r.Name)
-	d.Set("become_enabled", r.BecomeEnabled)
-	d.Set("use_fact_cache", r.UseFactCache)
 	d.Set("playbook", r.Playbook)
 	d.Set("project_id", r.Project)
+	d.Set("scm_branch", r.ScmBranch)
 	d.Set("skip_tags", r.SkipTags)
 	d.Set("start_at_task", r.StartAtTask)
 	d.Set("survey_enabled", r.SurveyEnabled)
+	d.Set("timeout", r.Timeout)
+	d.Set("use_fact_cache", r.UseFactCache)
+	d.Set("vault_credential_id", r.VaultCredential)
 	d.Set("verbosity", r.Verbosity)
+	d.Set("webhook_credential_id", r.WebhookCredential)
+	d.Set("webhook_service", r.WebhookService)
 	extraIDs := getExtraIDs(r)
 	d.Set("extra_credential_ids", extraIDs)
 	return d
