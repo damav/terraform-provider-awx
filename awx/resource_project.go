5aa4dda73700f280a68efa85381ff147a284cb25
David Fischer <david@fisch3r.net>
2020-04-07 14:05:00 +0200

    Refresh fields of resource_project

--- awx/resource_project.go
+++ awx/resource_project.go
@@ -22,53 +22,63 @@ func resourceProjectObject() *schema.Resource {
 				Required:    true,
 				Description: "Name of this project",
 			},
-
 			"description": &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     "",
 				Description: "Optional description of this project.",
 			},
-
 			"local_path": &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     "",
 				Description: "Local path (relative to PROJECTS_ROOT) containing playbooks and related files for this project.",
 			},
-
 			"scm_type": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
-				Description: "One of \"\" (manual), git, hg, svn",
+				Description: "Specifies the source control system used to store the project (one of '', git, hg, svn, insights).",
 			},
-
 			"scm_url": &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     "",
-				Description: "",
+				Description: "The location where the project is stored.",
 			},
-
 			"scm_branch": &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     "",
 				Description: "Specific branch, tag or commit to checkout.",
 			},
+			"scm_refspec": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "",
+				Description: "For git projects, an additional refspec to fetch.",
+			},
 			"scm_clean": &schema.Schema{
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Discard any local changes before syncing the project.",
 			},
 			"scm_delete_on_update": &schema.Schema{
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Delete the project before syncing.",
 			},
 			"credential_id": &schema.Schema{
-				Type:     schema.TypeInt,
-				Optional: true,
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Description: "Numeric ID of the project credential",
+			},
+			"timeout": &schema.Schema{
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     0,
+				Description: "The amount of time (in seconds) to run before the task is canceled.",
 			},
 			"organization_id": &schema.Schema{
 				Type:        schema.TypeInt,
@@ -76,14 +86,28 @@ func resourceProjectObject() *schema.Resource {
 				Description: "Numeric ID of the project organization",
 			},
 			"scm_update_on_launch": &schema.Schema{
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Update the project when a job is launched that uses the project.",
 			},
 			"scm_update_cache_timeout": &schema.Schema{
-				Type:     schema.TypeInt,
-				Optional: true,
-				Default:  0,
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     0,
+				Description: "The number of seconds after the last project update ran that a new project update will be launched as a job dependency.",
+			},
+			"allow_override": &schema.Schema{
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Allow changing the SCM branch or revision in a job template that uses this project.",
+			},
+			"custom_virtualenv": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     nil,
+				Description: "Local absolute file path containing a custom Python virtualenv to use",
 			},
 		},
 		Importer: &schema.ResourceImporter{
@@ -121,12 +145,16 @@ func resourceProjectCreate(d *schema.ResourceData, m interface{}) error {
 		"scm_type":                 d.Get("scm_type").(string),
 		"scm_url":                  d.Get("scm_url").(string),
 		"scm_branch":               d.Get("scm_branch").(string),
+		"scm_refspec":              d.Get("scm_refspec").(string),
 		"scm_clean":                d.Get("scm_clean").(bool),
 		"scm_delete_on_update":     d.Get("scm_delete_on_update").(bool),
 		"credential":               d.Get("credential_id").(int),
+		"timeout":                  d.Get("timeout").(int),
 		"organization":             d.Get("organization_id").(int),
 		"scm_update_on_launch":     d.Get("scm_update_on_launch").(bool),
 		"scm_update_cache_timeout": d.Get("scm_update_cache_timeout").(int),
+		"allow_override":           d.Get("allow_override").(bool),
+		"custom_virtualenv":        d.Get("custom_virtualenv").(string),
 	}, map[string]string{})
 	if err != nil {
 		return err
@@ -161,12 +189,16 @@ func resourceProjectUpdate(d *schema.ResourceData, m interface{}) error {
 		"scm_type":                 d.Get("scm_type").(string),
 		"scm_url":                  d.Get("scm_url").(string),
 		"scm_branch":               d.Get("scm_branch").(string),
+		"scm_refspec":              d.Get("scm_refspec").(string),
 		"scm_clean":                d.Get("scm_clean").(bool),
 		"scm_delete_on_update":     d.Get("scm_delete_on_update").(bool),
 		"credential":               d.Get("credential_id").(int),
+		"timeout":                  d.Get("timeout").(int),
 		"organization":             d.Get("organization_id").(int),
 		"scm_update_on_launch":     d.Get("scm_update_on_launch").(bool),
 		"scm_update_cache_timeout": d.Get("scm_update_cache_timeout").(int),
+		"allow_override":           d.Get("allow_override").(bool),
+		"custom_virtualenv":        d.Get("custom_virtualenv").(string),
 	}, map[string]string{})
 	if err != nil {
 		return err
@@ -238,14 +270,19 @@ func resourceProjectDelete(d *schema.ResourceData, m interface{}) error {
 func setProjectResourceData(d *schema.ResourceData, r *awxgo.Project) *schema.ResourceData {
 	d.Set("name", r.Name)
 	d.Set("description", r.Description)
+	d.Set("local_path", r.LocalPath)
 	d.Set("scm_type", r.ScmType)
 	d.Set("scm_url", r.ScmURL)
 	d.Set("scm_branch", r.ScmBranch)
+	d.Set("scm_refspec", r.ScmRefSpec)
 	d.Set("scm_clean", r.ScmClean)
 	d.Set("scm_delete_on_update", r.ScmDeleteOnUpdate)
 	d.Set("credential_id", r.Credential)
+	d.Set("timeout", r.Timeout)
 	d.Set("organization_id", r.Organization)
 	d.Set("scm_update_on_launch", r.ScmUpdateOnLaunch)
 	d.Set("scm_update_cache_timeout", r.ScmUpdateCacheTimeout)
+	d.Set("allow_override", r.AllowOverride)
+	d.Set("custom_virtualenv", r.CustomVirtualenv)
 	return d
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/davidfischer-ch/terraform-provider-awx
 go 1.12
 
 require (
-	github.com/davidfischer-ch/awx-go v0.2.1
+	github.com/davidfischer-ch/awx-go v0.2.2
 	github.com/hashicorp/terraform v0.12.6
 	gopkg.in/yaml.v2 v2.2.5
 )
